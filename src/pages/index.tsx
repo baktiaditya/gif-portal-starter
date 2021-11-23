639dcf29975fb5581daf31f6aa45c82a14110e2f
Bakti Aditya <bakti@pintu.co.id>
2021-11-23 07:19:58 +0700

    add wallet button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,133 @@
+import React from 'react';
 import { NextPage } from 'next';
+import { TWITTER_HANDLE, TWITTER_LINK, TEST_GIFS } from 'src/constants';
 import { Twemoji } from 'src/components';
 import createStyles from 'src/styles/home-page';
 import twitterLogo from 'src/assets/twitter-logo.svg';
 
-// Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+type CustomWindowType = Window &
+  typeof globalThis & { solana?: { isPhantom: boolean; isConnected: boolean; connect: Function } };
 
 const HomePage: NextPage = () => {
   const styles = createStyles();
 
+  const [walletAddress, setWalletAddress] = React.useState<string | null>(null);
+  const [inputValue, setInputValue] = React.useState('');
+  const [gifList, setGifList] = React.useState<Array<string>>([]);
+
+  /**
+   * When our component first mounts, let's check to see if we have a connected
+   * Phantom Wallet
+   */
+  React.useEffect(() => {
+    const onLoad = async () => {
+      await connectWallet();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+  /**
+   * Fetch gifs
+   */
+  React.useEffect(() => {
+    if (walletAddress) {
+      console.log('Fetching GIF list...');
+
+      // Call Solana program here.
+
+      // Set state
+      setGifList(TEST_GIFS);
+    }
+  }, [walletAddress]);
+
+  /**
+   * Let's define this method so our code doesn't break.
+   * We will write the logic for this next!
+   */
+  const connectWallet = async () => {
+    try {
+      const { solana } = window as CustomWindowType;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found!');
+
+          /*
+           * The solana object gives us a function that will allow us to connect
+           * directly with the user's wallet!
+           */
+          const response = await solana.connect();
+          console.log('Connected with Public Key:', response.publicKey.toString());
+
+          /*
+           * Set the user's publicKey in state to be used later!
+           */
+          setWalletAddress(response.publicKey.toString());
+        }
+      } else {
+        console.warn('Solana object not found! Get a Phantom Wallet 👻');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setInputValue(value);
+  };
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (inputValue.length > 0) {
+      console.log('Gif link:', inputValue);
+    } else {
+      console.log('Empty input. Try again.');
+    }
+  };
+
   return (
     <div css={styles.app}>
       {/* Header */}
-      <div css={styles.headerContainer}>
-        <p css={styles.header}>
-          <Twemoji size={54} emoji="🖼" /> GIF Portal
-        </p>
-        <p css={styles.subText}>
-          View your GIF collection in the metaverse <Twemoji size={24} emoji="✨" />
-        </p>
+      <div css={[styles.headerContainer, !walletAddress && styles.headerContainerEmpty]}>
+        <div css={styles.headerContainerInner}>
+          <p css={styles.header}>
+            <Twemoji size={54} emoji="🖼" /> GIF Portal
+          </p>
+          <p css={styles.subText}>
+            View your GIF collection in the metaverse <Twemoji size={24} emoji="✨" />
+          </p>
+          {!walletAddress && (
+            <button onClick={connectWallet} css={[styles.ctaButton, styles.connectWalletButton]}>
+              Connect to Wallet
+            </button>
+          )}
+        </div>
+
+        {/* Gif list */}
+        {walletAddress && (
+          <div css={styles.connectedContainer}>
+            <form onSubmit={handleFormSubmit}>
+              <input
+                type="text"
+                placeholder="Enter gif link!"
+                value={inputValue}
+                onChange={handleInputChange}
+              />
+              <button type="submit" css={[styles.ctaButton, styles.submitGifButton]}>
+                Submit
+              </button>
+            </form>
+            <div css={styles.gifGrid}>
+              {gifList.map(gif => (
+                <div key={gif} css={styles.gifItem}>
+                  <img src={gif} alt={gif} />
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
 
       {/* Footer */}
--- src/styles/home-page.ts
+++ src/styles/home-page.ts
@@ -1,22 +1,33 @@
 import { css } from '@emotion/react';
 
 export default function createStyles() {
+  const footerHeight = 114;
+
   return {
     app: css`
       display: flex;
+      flex-direction: column;
       align-items: center;
       justify-content: center;
-      height: 100vh;
+      min-height: 100vh;
     `,
-    authedContainer: css`
-      height: 100%;
+
+    headerContainer: css`
       display: flex;
       flex-direction: column;
+      justify-content: center;
       padding: 30px;
+      padding-top: 60px;
     `,
-    headerContainer: css`
-      padding: 0 30px 0 30px;
-      text-align: center;
+    headerContainerEmpty: css`
+      flex: 1;
+      padding-top: 30px;
+      padding-top: ${footerHeight}px;
+    `,
+    headerContainerInner: css`
+      display: flex;
+      flex-direction: column;
+      align-items: center;
     `,
     header: css`
       margin: 0;
@@ -28,8 +39,86 @@ export default function createStyles() {
       font-size: 25px;
       color: white;
     `,
+
+    connectedContainer: css`
+      padding: 30px 30px 0;
+
+      form {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        margin-top: 10px;
+        margin-bottom: 50px;
+
+        input[type='text'] {
+          flex: 1;
+          color: white;
+          padding: 10px;
+          width: 50%;
+          height: 50px;
+          font-size: 16px;
+          box-sizing: border-box;
+          background-color: rgba(0, 0, 0, 0.25);
+          border: none;
+          border-radius: 10px;
+        }
+
+        button {
+          height: 50px;
+        }
+      }
+    `,
+    gifGrid: css`
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      grid-gap: 1.5rem;
+    `,
+    gifItem: css`
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      justify-self: center;
+      align-self: center;
+
+      img {
+        width: 100%;
+        height: 300px;
+        border-radius: 10px;
+        object-fit: cover;
+      }
+    `,
+    connectWalletButton: css`
+      background: -webkit-linear-gradient(left, #60c657, #35aee2);
+      background-size: 200% 200%;
+    `,
+    submitGifButton: css`
+      background: -webkit-linear-gradient(left, #4e44ce, #35aee2);
+      background-size: 200% 200%;
+      margin-left: 10px;
+    `,
+
+    footerContainer: css`
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      margin-top: auto;
+      height: ${footerHeight}px;
+      padding: 0 30px;
+    `,
+    twitterLogo: css`
+      width: 24px;
+      height: auto;
+      margin-right: 4px;
+    `,
+    footerText: css`
+      color: white;
+      font-size: 16px;
+      font-weight: bold;
+    `,
+
     gradientText: css`
-      background: linear-gradient(left, #60c657 30%, #35aee2 60%);
+      background: -webkit-linear-gradient(left, #60c657 30%, #35aee2 60%);
       background-clip: text;
     `,
     ctaButton: css`
@@ -43,78 +132,6 @@ export default function createStyles() {
       font-size: 16px;
       font-weight: bold;
       color: white;
-    `,
-    connectWalletButton: css`
-      background: linear-gradient(left, #60c657, #35aee2);
-      background-size: 200% 200%;
-      animation: gradient-animation 4s ease infinite;
-    `,
-    submitGifButton: css`
-      background: linear-gradient(left, #4e44ce, #35aee2);
-      background-size: 200% 200%;
-      animation: gradient-animation 4s ease infinite;
-      margin-left: 10px;
-    `,
-    footerContainer: css`
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      position: absolute;
-      width: 100%;
-      bottom: 0;
-      left: 0;
-      padding: 0 30px 45px 30px;
-    `,
-    twitterLogo: css`
-      width: 24px;
-      height: auto;
-      margin-right: 4px;
-    `,
-    footerText: css`
-      color: white;
-      font-size: 16px;
-      font-weight: bold;
-    `,
-    gifGrid: css`
-      display: grid;
-      grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
-      grid-gap: 1.5rem;
-      justify-items: center;
-      margin: 0;
-      padding: 0;
-    `,
-    gifItem: css`
-      display: flex;
-      flex-direction: column;
-      position: relative;
-      justify-self: center;
-      align-self: center;
-
-      img {
-        width: 100%;
-        height: 300px;
-        border-radius: 10px;
-        object-fit: cover;
-      }
-    `,
-    connectedContainer: css`
-      input[type='text'] {
-        display: inline-block;
-        color: white;
-        padding: 10px;
-        width: 50%;
-        height: 60px;
-        font-size: 16px;
-        box-sizing: border-box;
-        background-color: rgba(0, 0, 0, 0.25);
-        border: none;
-        border-radius: 10px;
-        margin: 50px auto;
-      }
-
-      button {
-        height: 50px;
-      }
     `,
   };
 }
